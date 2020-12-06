96927b96d3eaf92aa3f817c703b50894b05bd076
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-12-06 22:34:36 +0100

    Generate docs.

---
+++ inject/script/0-header.js
@@ -0,0 +1,30 @@
+const domReady = function (callback) {
+	const state = document.readyState;
+	if (state === 'interactive' || state === 'complete') {
+		callback();
+	} else {
+		document.addEventListener('DOMContentLoaded', callback);
+	}
+};
+
+domReady(function () {
+	const projectname = document.createElement('a');
+	projectname.classList.add('project-name');
+	projectname.text = 'aureooms/js-pseudo-random';
+	projectname.href = './index.html';
+
+	const header = document.querySelectorAll('header')[0];
+	header.insertBefore(projectname, header.firstChild);
+
+	const testlink = document.querySelector('header > a[data-ice="testLink"]');
+	testlink.href = 'https://coveralls.io/github/aureooms/js-pseudo-random';
+	testlink.target = '_BLANK';
+
+	const searchBox = document.querySelector('.search-box');
+	const input = document.querySelector('.search-input');
+
+	// Active search box when focus on searchBox.
+	input.addEventListener('focus', function () {
+		searchBox.classList.add('active');
+	});
+});
