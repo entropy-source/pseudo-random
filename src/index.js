b30ab44617741da9a0913c6e3cdd79b1afbed8ca
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-07-15 13:10:03 +0200

    :boom: refactor!: Use flat export style.
    BREAKING CHANGE: The default export is gone.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import fill from './fill.js';
-import nextFloat64 from './nextFloat64.js';
-import nextInt32 from './nextInt32.js';
-import nextUint64 from './nextUint64.js';
-import splitmix64 from './splitmix64.js';
-import xoroshiro128plus from './xoroshiro128plus.js';
-
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-export default {
-	fill,
-	nextFloat64,
-	nextInt32,
-	nextUint64,
-	splitmix64,
-	xoroshiro128plus,
-};
-
-export {fill, nextFloat64, nextInt32, nextUint64, splitmix64, xoroshiro128plus};
+export {default as _fill} from './_fill.js';
+export {default as fill} from './fill.js';
+export {default as genBit} from './genBit.js';
+export {default as genBoolean} from './genBoolean.js';
+export {default as genByte} from './genByte.js';
+export {default as genInt32} from './genInt32.js';
+export {default as genUint16} from './genUint16.js';
+export {default as genUint64} from './genUint64.js';
+export {default as nextBigInt64} from './nextBigInt64.js';
+export {default as nextBigUint64} from './nextBigUint64.js';
+export {default as nextFloat32} from './nextFloat32.js';
+export {default as nextFloat64} from './nextFloat64.js';
+export {default as nextInt32} from './nextInt32.js';
+export {default as nextUint16} from './nextUint16.js';
+export {default as nextUint32} from './nextUint32.js';
+export {default as nextUint64} from './nextUint64.js';
+export {default as nextUint8} from './nextUint8.js';
+export {default as splitmix64} from './splitmix64.js';
+export {default as xoroshiro128plus} from './xoroshiro128plus.js';
